e82a775f15fd05b320becfc7332bf2dd97bf7b93
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-07-19 18:34:27 +0200

    m

--- src/Components/CurrentBalance/CurrentBalance.js
+++ src/Components/CurrentBalance/CurrentBalance.js
@@ -31,7 +31,7 @@ const CurrentBalance = ({ movementsTable }) => {
             lg={{ span: 1, offset: 1 }}
             xs={{ span: 3, offset: 4 }}
           >
-            <CurrencyDollar className="mb-1 current-title" />
+            <CurrencyDollar className="mb-1" />
             <div className=" current-title">{toString}</div>
           </Col>
         </Row>
